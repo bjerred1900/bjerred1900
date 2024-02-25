b6e2b980b059bd14a5d24e3f15d6d0e51155b81a
bjerred1900 <161215023+bjerred1900@users.noreply.github.com>
2024-02-25 16:42:51 +0100

    Fixed filename lower case on images
    ---------
    
    Co-authored-by: Adrian Hansson <adense13@gmail.com>

--- scripts/lower_case_filenames.py
+++ scripts/lower_case_filenames.py
@@ -2,11 +2,16 @@
 import os
 import re
 
-def lowercase_markdown_filenames() -> None:
+def lowercase_filenames() -> None:
     """Scan each Markdown (.md) file in the root directory. Ensure each file name is lowercase."""
     for file in os.listdir():
         if file.endswith('.md'):
             os.rename(file, file.lower())
 
+    # also check for each jpg, jpeg, png, gif, and svg file in the images directory
+    for file in os.listdir('images'):
+        if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.png') or file.endswith('.gif') or file.endswith('.svg'):
+            os.rename(f'images/{file}', f'images/{file.lower()}')
+
 if __name__ == '__main__':
-    lowercase_markdown_filenames()
+    lowercase_filenames()
